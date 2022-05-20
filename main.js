7d34251641aeb3c2beae2acc9e61ae7a1abd6e7c
Sandra Müller <muelsand20@gmail.com>
2022-05-20 14:42:15 +0200

    added delete functionality

--- main.js
+++ main.js
@@ -7,55 +7,66 @@ today = dd + '.' + mm + '.' + yyyy;
 document.getElementById('date').innerHTML = today;
 
 
+//--------------------------------------------------------------------------------
 
+function uid() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
 
-
-
-
+//console.log(uid());
 
 const newTask = document.getElementById('new-task-input');
 const addButton = document.getElementById('button-addon2');
 const list = document.getElementById('task-container');
 //console.log(newTask.value);
 
-function addTask () {
+function addTask (arr) {
     if(newTask.value === '') {
-        alert('Fill in the Task!!!!!');
+        alert('Fill in the Task!');
     } else {
         document.querySelector('#task-container').innerHTML +=
-    `
-    <div class="col-10">
-        <div class="input-group mb-3 p-3">
-            <div class="input-group-text">
-                <input class="form-check-input id="checkbox" mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
+        `
+        <div class="row border rounded p-3 d-flex align-items-center">
+            <div class="col-10">
+                <div class="input-group mb-3 p-3">
+                    <div class="input-group-text">
+                        <input class="form-check-input id="checkbox" mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
+                    </div>
+                    <input type="text" class="form-control" id="input" value=${newTask.value} readonly aria-label="Text input with checkbox">
+                </div>
             </div>
-            <input type="text" class="form-control" id="input" value=${newTask.value} readonly aria-label="Text input with checkbox">
-        </div>
-    </div>
-    <div class="col-1">
-        <button class="btn btn-outline-secondary mb-3">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-            </svg>
-        </button>
-    </div>
-    <div class="col-1">
-        <button class="btn btn-outline-secondary mb-3" id="delete-Button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-            </svg>
-        </button>
-    </div>
-    `
-    
-    const deleteButton = document.getElementById('delete-Button');
+            <div class="col-1">
+                <button class="btn btn-outline-secondary mb-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="col-1">
+                <button class="btn btn-outline-secondary mb-3 delete-Button" id=${uid()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                    </svg>
+                </button>
+            </div>
+        <div>
+        `
 
-    deleteButton.addEventListener("click", () => {
-        setTimeout(() => {
-            document.querySelector('#task-container').removeChild('div');
-        }, 100);
-      });
-    }
+        const deleteButton = document.querySelectorAll('.delete-Button');
+
+        const deleteFunction = (id) => (e) => {
+            const buttonToDelete = document.getElementById(id);
+            buttonToDelete.parentElement.parentElement.remove();
+        }
+
+        deleteButton.forEach(button => {
+            const deleteFunctionId = deleteFunction(button.getAttribute('id'));
+            button.addEventListener('click', deleteFunctionId);
+        });
+    } 
 }
-addButton.addEventListener('click', addTask);
+
+addButton.addEventListener("click", addTask);
